d02741f21a1f97d329d023a96e0bfba9b342baad
Cyan Villarin <cyanvillarin@gmail.com>
2022-02-25 12:12:58 +0900

    Implement unit testing for Observable properties

--- UnifaCodingExamTests/SearchViewModelTests.swift
+++ UnifaCodingExamTests/SearchViewModelTests.swift
@@ -8,35 +8,64 @@
 import XCTest
 import RxTest
 import RxSwift
-import RxCocoa
+
+/// NOTE: In order to unit test observables, I needed to use the RxTest framework. Since Observables are not just
+/// ordinary values, but are streams of data, using XCTest only would not be enough.
+/// Reference: https://betterprogramming.pub/rxswift-unit-testing-explained-in-3-minutes-c024b7a26d
 
 @testable import UnifaCodingExam
 
 class SearchViewModelTests: XCTestCase {
    
    var viewModel: SearchViewModel!  /// system under test
-   var apiService: ApiService! = MockApiService()
+   var mockApiService: MockApiService!
    var disposeBag: DisposeBag!
    var testScheduler: TestScheduler! /// main element of RxTests
    
    override func setUp() {
       super.setUp()
       self.disposeBag = DisposeBag()
-      self.apiService = MockApiService()
-      self.viewModel = SearchViewModel(apiService: self.apiService)
+      self.mockApiService = MockApiService()
+      self.viewModel = SearchViewModel(apiService: self.mockApiService)
       self.testScheduler = TestScheduler(initialClock: 0)
    }
    
    override func tearDown() {
       self.disposeBag = nil
       self.viewModel = nil
-      self.apiService = nil
+      self.mockApiService = nil
       self.testScheduler = nil
       super.tearDown()
    }
    
    func test_fetchPhotos() async {
+      
+      /// create testSchedulers
+      let photos = testScheduler.createObserver([Photo].self)
+      let shouldShowNoResultsView = testScheduler.createObserver(Bool.self)
+      let shouldShowLoadingView = testScheduler.createObserver(Bool.self)
+      let currentPageNumber = testScheduler.createObserver(Int.self)
+      
+      /// bind
+      viewModel.photos.bind(to: photos).disposed(by: disposeBag)
+      viewModel.shouldShowNoResultsView.bind(to: shouldShowNoResultsView).disposed(by: disposeBag)
+      viewModel.shouldShowLoadingView.bind(to: shouldShowLoadingView).disposed(by: disposeBag)
+      viewModel.currentPageNumber.bind(to: currentPageNumber).disposed(by: disposeBag)
+      
+      /// check if Observables don't have any values yet
+      XCTAssertRecordedElements(photos.events, [])
+      XCTAssertRecordedElements(shouldShowNoResultsView.events, [])
+      XCTAssertRecordedElements(shouldShowLoadingView.events, [])
+      XCTAssertRecordedElements(currentPageNumber.events, [])
+      
+      /// perform the simulation of fetching photos
       await viewModel.fetchPhotos(queryKeyword: "test")
+      
+      /// check if Observables are updated with the latest value fetched from viewModel.fetchPhotos
+      XCTAssertRecordedElements(photos.events, [mockApiService.mockResponse.photos])
+      XCTAssertRecordedElements(shouldShowNoResultsView.events, [false])
+      XCTAssertRecordedElements(shouldShowLoadingView.events, [true, false])
+      XCTAssertRecordedElements(currentPageNumber.events, [1])
       
       /// check if pageNumber is set to 1 when fetchPhotos is called
       XCTAssertEqual(viewModel.pageNumber, 1)
